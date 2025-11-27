52f34a4ed73b69e6042d228e48c6023a31eeb55f
CrowmanXD <tudor.arsene@student.unitbv.ro>
2025-11-27 06:42:10 +0200

    2nd azure lab changes

--- src/HCI.AIAssistant.API/Program.cs
+++ src/HCI.AIAssistant.API/Program.cs
@@ -1,6 +1,43 @@
+using Azure.Identity;
+using HCI.AIAssistant.API.Managers;
+using HCI.AIAssistant.API.Models.CustomTypes;
+using HCI.AIAssistant.API.Services;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Replace appsettings.json values with Key Vault values 
+
+var keyVaultName = builder.Configuration["AppConfigurations:KeyVaultName"];
+var secretsPrefix = builder.Configuration["AppConfigurations:SecretsPrefix"];
+if (string.IsNullOrWhiteSpace(keyVaultName))
+{
+    throw new ArgumentNullException("KeyVaultName", "KeyVaultName is missing in AppConfigurations.");
+}
+if (string.IsNullOrWhiteSpace(secretsPrefix))
+{
+    throw new ArgumentNullException("SecretsPrefix", "SecretsPrefix is missing in AppConfigurations.");
+}
+var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(
+    keyVaultUri,
+    new DefaultAzureCredential(),
+    new CustomSecretManager(secretsPrefix)
+);
+
+// Configure values based on appsettings.json 
+builder.Services.Configure<SecretsService>(builder.Configuration.GetSection("Secrets")); 
+builder.Services.Configure<AppConfigurationsService>(builder.Configuration.GetSection("AppConfigurations")); 
+ 
+// Add services to the container. 
+builder.Services.AddSingleton<ISecretsService>( 
+    provider => provider.GetRequiredService<IOptions<SecretsService>>().Value 
+); 
+builder.Services.AddSingleton<IAppConfigurationsService>( 
+    provider => provider.GetRequiredService<IOptions<AppConfigurationsService>>().Value 
+); 
+ 
+builder.Services.AddSingleton<IParametricFunctions, ParametricFunctions>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,4 +62,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.EndPoint); 
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.Key); 
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.Id); 
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.IoTHubSecrets?.ConnectionString); 
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.KeyVaultName); 
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.SecretsPrefix); 
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.IoTDeviceName); 
+app.Run(); 
